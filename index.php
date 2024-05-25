b1c8073ede82a85d0ab3f2dae50538e5648c5324
xChezY <it@leonphan.dev>
2024-05-25 16:05:55 +0200

    Daten in Tabelle anzeigen

--- index.php
+++ index.php
@@ -1,35 +1,86 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>PlanMonitor</title>
-    </head>
-    <body>
-        <pre>
-            <?php
 
-            require_once realpath(__DIR__ . '/vendor/autoload.php');
-            use Dotenv\Dotenv;
-            $dotenv = Dotenv::createImmutable(__DIR__);
-            $dotenv->load();
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PlanMonitor</title>
+</head>
 
-            $client = new GuzzleHttp\Client();
-            $url = $_ENV['INFLUX_DB_URL'];
-            $key = $_ENV['INFLUX_DB_API_KEY'];
-            $res = $client->request('POST', $url . "/api/v2/query",
-                [
-                    "headers" => [
-                        "Authorization" => "Token ". $key,
-                        "Content-Type" => "application/vnd.flux"
-                    ],
-                    "body" => 'from(bucket: "ttn_vhs")
-                    |> range(start: -24h)
-                    |> filter(fn: (r) => r._measurement == "ttn_vhs")'
-                ]);
-            echo $res->getBody();
-            
+<body>
+    <table>
+        <?php
+
+        require_once realpath(__DIR__ . '/vendor/autoload.php');
+        use Dotenv\Dotenv;
+
+        $dotenv = Dotenv::createImmutable(__DIR__);
+        $dotenv->load();
+
+        $client = new GuzzleHttp\Client();
+        $url = $_ENV['INFLUX_DB_URL'];
+        $key = $_ENV['INFLUX_DB_API_KEY'];
+        $bucket = $_ENV["BUCKET"];
+        $res = $client->request(
+            'POST',
+            $url . "/api/v2/query",
+            [
+                "headers" => [
+                    "Authorization" => "Token " . $key,
+                    "Content-Type" => "application/vnd.flux"
+                ],
+                "body" => 'from(bucket: "' . $bucket . '") |> range(start: -20m) |> filter(fn: (r) => r._measurement == "ttn_vhs")'
+            ]
+        );
+
+        $result_as_string = $res->getBody();
+        $rows = explode("\n", $result_as_string);
+        //$array = explode("\n,result,table,_start,_stop,_time,_value,_field,_measurement,device_id,host,topic", $res->getBody());
+        //var_dump($rows);
+        
+
+
+        foreach ($rows as $row) {
+            $cells = explode(",", $row);
+            $is_header = str_starts_with($cells[1], "result");
+
             ?>
-        </pre>
-    </body>
+            <tr>
+                <?php
+                foreach ($cells as $cell) {
+                    if ($is_header) {
+                        ?>
+                        <th>
+                            <?php
+                            $cell ?>
+                        </th>
+                        <?php
+                    } else {
+                        ?>
+                        <td>
+                            <?php
+                            $cell ?>
+                        </td>
+                        <?php
+                    }
+
+                }
+                ?>
+            </tr>
+            <?php
+        }
+
+
+
+        ?>
+    </table>
+    <table>
+        <tr>
+            <th>Company</th>
+            <th>Contact</th>
+            <th>Country</th>
+        </tr>
+    </table>
+</body>
+
 </html>
