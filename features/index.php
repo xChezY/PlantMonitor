f964c707bf66794ecfb199075be0a1678db5ba8b
Leon Phan <42874436+xXChezyXx@users.noreply.github.com>
2024-06-10 21:35:40 +0200

    adding bulma

---
+++ features/index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PlanMonitor</title>
+</head>
+<style>
+    table, th, td {
+  border: 1px solid;
+}
+
+th, td {
+  padding: 10px;
+  text-align: left;
+}
+</style>
+
+<body>
+        <?php
+
+        require_once realpath(dirname(__DIR__, 1) . '/vendor/autoload.php');
+        use Dotenv\Dotenv;
+
+        $dotenv = Dotenv::createImmutable(dirname(__DIR__, 1));
+        $dotenv->load();
+
+        $client = new GuzzleHttp\Client();
+        $url = $_ENV['INFLUX_DB_URL'];
+        $key = $_ENV['INFLUX_DB_API_KEY'];
+        $bucket = $_ENV["BUCKET"];
+        $res = $client->request(
+            'POST',
+            $url . "/api/v2/query",
+            [
+                "headers" => [
+                    "Authorization" => "Token " . $key,
+                    "Content-Type" => "application/vnd.flux"
+                ],
+                "body" => '
+                import "strings"
+                from(bucket: "' . $bucket . '")
+                |> range(start: -10h)
+                |> filter(fn: (r) => contains(value: r._field, set: ["temp_SOIL","water_SOIL","conduct_SOIL"]))
+                '
+            ]
+        );
+
+        $remove_attribute = [0,1,8,9,10,11];
+        $result_as_string = $res->getBody();
+        $rows = explode("\n", $result_as_string);
+        $counter = 0;
+        $current = "";
+        $header_array = explode(",",$rows[0]);
+        foreach($remove_attribute as $index){
+            unset($header_array[$index]);
+        }
+        $header_table = "<tr><th>".implode("</th><th>",$header_array)."</th></tr>";
+        for($i = 0; $i < count($rows); $i++){
+            $cells = explode(",", $rows[$i]);
+            if (count($cells) == 1 || $cells[2] == "table"){
+                unset($rows[$i]);
+                continue;
+            }
+            if($current == $cells[2] || $current == ""){
+                $current = $cells[2];
+                $counter++;
+                continue;
+            }
+            
+        }
+        $rows = array_values($rows);
+        for($i = 0; $i < $counter; $i++){
+            echo "<h3>Pflanze Nr.".$i."</h3><table>".$header_table;
+            echo count($rows);
+            for($j = 0; $i+$j < count($rows); $j+=$counter){
+                $order = $rows[$i+$j];
+                $cells = explode(",", $order);
+                foreach($remove_attribute as $index){
+                    unset($cells[$index]);
+                }
+                echo "<tr>";
+                foreach ($cells as $cell) {
+                    echo "<td>" . $cell . "</td>";
+                }
+                echo "</tr>";
+            }
+            echo "</table>";
+        }
+
+        ?>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -4,87 +4,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PlanMonitor</title>
+    <title>Homepage</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.0/css/bulma.min.css">
 </head>
-<style>
-    table, th, td {
-  border: 1px solid;
-}
-
-th, td {
-  padding: 10px;
-  text-align: left;
-}
-</style>
 
 <body>
-        <?php
-
-        require_once realpath(__DIR__ . '/vendor/autoload.php');
-        use Dotenv\Dotenv;
-
-        $dotenv = Dotenv::createImmutable(__DIR__);
-        $dotenv->load();
-
-        $client = new GuzzleHttp\Client();
-        $url = $_ENV['INFLUX_DB_URL'];
-        $key = $_ENV['INFLUX_DB_API_KEY'];
-        $bucket = $_ENV["BUCKET"];
-        $res = $client->request(
-            'POST',
-            $url . "/api/v2/query",
-            [
-                "headers" => [
-                    "Authorization" => "Token " . $key,
-                    "Content-Type" => "application/vnd.flux"
-                ],
-                "body" => 'from(bucket: "' . $bucket . '") |> range(start: -10h)  |> filter(fn: (r) => r._measurement == "' . $bucket . '")'
-            ]
-        );
-
-        $remove_attribute = [0,1,8,9,10,11];
-        $result_as_string = $res->getBody();
-        $rows = explode("\n", $result_as_string);
-        $counter = 0;
-        $current = "";
-        $header_array = explode(",",$rows[0]);
-        foreach($remove_attribute as $index){
-            unset($header_array[$index]);
-        }
-        $header_table = "<tr><th>".implode("</th><th>",$header_array)."</th></tr>";
-        for($i = 0; $i < count($rows); $i++){
-            $cells = explode(",", $rows[$i]);
-            if (count($cells) == 1 || $cells[2] == "table"){
-                unset($rows[$i]);
-                continue;
-            }
-            if($current == $cells[2] || $current == ""){
-                $current = $cells[2];
-                $counter++;
-                continue;
-            }
-            
-        }
-        $rows = array_values($rows);
-        for($i = 0; $i < $counter; $i++){
-            echo "<h3>Pflanze Nr.".$i."</h3><table>".$header_table;
-            echo count($rows);
-            for($j = 0; $i+$j < count($rows); $j+=$counter){
-                $order = $rows[$i+$j];
-                $cells = explode(",", $order);
-                foreach($remove_attribute as $index){
-                    unset($cells[$index]);
-                }
-                echo "<tr>";
-                foreach ($cells as $cell) {
-                    echo "<td>" . $cell . "</td>";
-                }
-                echo "</tr>";
-            }
-            echo "</table>";
-        }
-
-        ?>
+    <section class="section">
+        <div class="container">
+            <h1 class="title">
+                PlanMonitor
+            </h1>
+            <p class="subtitle">
+                Monitor your plans
+            </p>
+            <progress class="progress is-danger" value="50" max="100">
+                15%
+            </progress>
+            <button class="button is-link">Virus installieren</button>
+        </div>
 </body>
 
 </html>
