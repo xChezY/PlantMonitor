4f328d124bc31eaf67e8df8e54ba496076c8e27d
Liontg6 <lionfielitz@gmail.com>
2024-06-11 22:37:50 +0200

    Feature Site redesign

--- contact/index.php
+++ contact/index.php
@@ -11,54 +11,12 @@
 <body>
     <?php include '../assets/navbar.php'; ?>
     <section class="section">
-        <style>
-            .container {
-                margin-left: 50px;
-            }
-
-            .subtitle {
-                margin-left: 30px;
-                margin-top: 90px;
-            }
-
-            .box {
-                margin-left: 50px;
-
-            }
-
-            .box1 {
-                margin-left: 0px;
-                height: 100px;
-                height: 80px;
-                width: 400px;
-
-            }
-
-            .box2 {
-                margin-left: 50px;
-                margin-top: 90px;
-                width: 700px;
-            }
-
-            .image2 img{
-                float: right;
-                margin-top: -650px;
-                margin-right: 200px;
-                width: 500px;
-                height: 500px;
-            }
-        </style>
         <div class="container">
-            <div class="box box1">
-                <h1 class="title">Kontaktieren Sie uns!</h1>
-            </div>
-
-            <h2 class="subtitle">textemail@mail.com</h2>
-            <h2 class="subtitle">Telefonnummer</h2>
-
-        </div>
-
-        <form class="box box2">
+        <div class="columns is-multiline">
+    <div class="column is-6">
+        <h2 class="subtitle">textemail@mail.com</h2>
+        <h2 class="subtitle">Telefonnummer</h2>
+        <form class="box">
             <div class="field">
                 <h5 class="title is-5">Nachrichten direkt senden</h5>
                 <div class="control">
@@ -73,9 +31,14 @@
 
             <button class="mt-2 button is-primary">Abschicken</button>
         </form>
-        <figure class="image image2">
+    </div>
+
+    <div class="column is-4">
+        <figure class="image">
             <img src="https://bulma.io/assets/images/placeholders/1280x960.png" />
         </figure>
+    </div>
+</div>
     </section>
     <?php include '../assets/footer.php'; ?>
 </body>
--- features/index.php
+++ features/index.php
@@ -5,96 +5,111 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PlanMonitor</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.0/css/bulma.min.css">
 </head>
-<style>
-    table, th, td {
-  border: 1px solid;
-}
-
-th, td {
-  padding: 10px;
-  text-align: left;
-}
-</style>
 
 <body>
-        <?php
+    <?php include '../assets/navbar.php'; ?>
+    <?php
 
-        require_once realpath(dirname(__DIR__, 1) . '/vendor/autoload.php');
-        use Dotenv\Dotenv;
+    require_once realpath(dirname(__DIR__, 1) . '/vendor/autoload.php');
+    use Dotenv\Dotenv;
 
-        $dotenv = Dotenv::createImmutable(dirname(__DIR__, 1));
-        $dotenv->load();
+    $dotenv = Dotenv::createImmutable(dirname(__DIR__, 1));
+    $dotenv->load();
 
-        $client = new GuzzleHttp\Client();
-        $url = $_ENV['INFLUX_DB_URL'];
-        $key = $_ENV['INFLUX_DB_API_KEY'];
-        $bucket = $_ENV["BUCKET"];
-        $res = $client->request(
-            'POST',
-            $url . "/api/v2/query",
-            [
-                "headers" => [
-                    "Authorization" => "Token " . $key,
-                    "Content-Type" => "application/vnd.flux"
-                ],
-                "body" => '
+    $client = new GuzzleHttp\Client();
+    $url = $_ENV['INFLUX_DB_URL'];
+    $key = $_ENV['INFLUX_DB_API_KEY'];
+    $bucket = $_ENV["BUCKET"];
+    $res = $client->request(
+        'POST',
+        $url . "/api/v2/query",
+        [
+            "headers" => [
+                "Authorization" => "Token " . $key,
+                "Content-Type" => "application/vnd.flux"
+            ],
+            "body" => '
                 import "strings"
                 from(bucket: "' . $bucket . '")
                 |> range(start: -10h)
                 |> filter(fn: (r) => contains(value: r._field, set: ["temp_SOIL","water_SOIL","conduct_SOIL"]))
                 '
-            ]
-        );
+        ]
+    );
 
-        $remove_attribute = [0,1,8,9,10,11];
-        $result_as_string = $res->getBody();
-        $rows = explode("\n", $result_as_string);
-        $counter = 0;
-        $current = "";
-        $header_array = explode(",",$rows[0]);
-        foreach($remove_attribute as $index){
-            unset($header_array[$index]);
+    $remove_attribute = [0, 1, 8, 9, 10, 11];
+    $result_as_string = $res->getBody();
+    $rows = explode("\n", $result_as_string);
+    $counter = 0;
+    $current = "";
+    $header_array = explode(",", $rows[0]);
+    foreach ($remove_attribute as $index) {
+        unset($header_array[$index]);
+    }
+    $header_table = "<tr><th>" . implode("</th><th>", $header_array) . "</th></tr>";
+    for ($i = 0; $i < count($rows); $i++) {
+        $cells = explode(",", $rows[$i]);
+        if (count($cells) == 1 || $cells[2] == "table") { // Remove unnecessary lines when count($cells) == 1 then the line is empty and has no content. 
+            unset($rows[$i]); // If $cells[2] == "table" then this is the header line, which sometimes repeats in the table.
+            continue;
         }
-        $header_table = "<tr><th>".implode("</th><th>",$header_array)."</th></tr>";
-        for($i = 0; $i < count($rows); $i++){   
-            $cells = explode(",", $rows[$i]);
-            if (count($cells) == 1 || $cells[2] == "table"){ // Remove unnecessary lines when count($cells) == 1 then the line is empty and has no content. 
-                unset($rows[$i]); // If $cells[2] == "table" then this is the header line, which sometimes repeats in the table.
-                continue;
-            }
-            if($current == $cells[2] || $current == ""){ //Goal: Find the number of rows per plant. As long as the first value exists. 
-                $current = $cells[2]; // If the table has a specific number, the number of times this number appears in the following rows is counted.
-                $counter++;
-                continue; // In the end, we can now say that the variable 'current' holds the number of rows for the respective plant.
-            }
-            
+        if ($current == $cells[2] || $current == "") { //Goal: Find the number of rows per plant. As long as the first value exists. 
+            $current = $cells[2];// If the table has a specific number, the number of times this number appears in the following rows is counted.
+            $counter++;
+            continue;// In the end, we can now say that the variable 'current' holds the number of rows for the respective plant.
         }
-        $pflanzen = array();
-        $rows = array_values($rows);
-        for($i = 0; $i < $counter; $i++){
-            echo "<h3>Pflanze Nr.".$i."</h3><table>".$header_table;
-            $pflanze = array();
-            for($j = 0; $i+$j < count($rows); $j+=$counter){ // Goal: Sort the table by plants instead of by attributes.
-                $order = $rows[$i+$j]; // Always iterate over the n-th element of the attributes and place them into a table.
-                $cells = explode(",", $order);
-                foreach($remove_attribute as $index){
-                    unset($cells[$index]);
-                }
-                $pflanze[] = $cells[6];
-                echo "<tr>";
-                foreach ($cells as $cell) {
-                    echo "<td>" . $cell . "</td>";
-                }
-                echo "</tr>";
-            }
-            $pflanzen[] = $pflanze; // Save the data from the fifth column of the plant into the array.
-            print_r($pflanzen);
-            echo "</table>";
-        }
-        print_r($pflanzen[1][0]); // Print the data from the fifth column of the second plant. DEBUGGING!
 
-        ?>
+    }
+    $pflanzen = array();
+    $rows = array_values($rows);
+    for ($i = 0; $i < $counter; $i++) {
+        $pflanze = array();
+        for ($j = 0; $i + $j < count($rows); $j += $counter) { // Goal: Sort the table by plants instead of by attributes.
+            $order = $rows[$i + $j]; // Always iterate over the n-th element of the attributes and place them into a table.
+            $cells = explode(",", $order);
+            foreach ($remove_attribute as $index) {
+                unset($cells[$index]);
+            }
+            $pflanze[] = $cells[6];
+        }
+        $pflanzen[] = $pflanze; // Save the data from the fifth column of the plant into the array.
+    }
+    print_r($pflanzen[1][0]); // Print the data from the fifth column of the second plant. DEBUGGING!
+    
+    ?>
+    <style>
+        .custom-margin {
+            margin-top: 10rem;
+        }
+    </style>
+
+
+    <h1 class="title has-text-centered">Sehen Sie ihre Pflanze ein!</h1>
+    <div class="columns custom-margin">
+        <div class="column is-half is-offset-one-quarter">
+            <form method="post">
+                <input name="myInput" class="input has-text-centered" type="text"
+                    placeholder="Welche Nummer hat ihre Pflanze?" />
+                <div style="text-align: center;">
+                    <button type="submit" class="button is-info mt-2">Info</button>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $inputValue = $_POST['myInput'];
+        echo "<h1 class='title has-text-centered'>Die Pflanze mit der Nummer $inputValue hat folgende Werte:</h1>";
+        echo "<h2 class='subtitle has-text-centered mt-6'>Bodenleitfähigkeit: " . $pflanzen[$inputValue][0] . " µS</h2>";
+        echo "<h2 class='subtitle has-text-centered'>Bodentemperatur: " . $pflanzen[$inputValue][1] . " °C</h2>";
+        echo "<h2 class='subtitle has-text-centered'>Bodenfeuchtigkeit: " . $pflanzen[$inputValue][2] . " cb (Zentibar)</h2>";
+    }
+    ?>
+
+    <?php include '../assets/footer.php'; ?>
 </body>
 
 </html>
