3a949cbc1e0cb0b6b0f97a03b4cdfd12248652bd
Leon Phan <42874436+xXChezyXx@users.noreply.github.com>
2024-09-05 13:20:35 +0200

    add timestamps

--- features/index.php
+++ features/index.php
@@ -6,14 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PlanMonitor</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.0/css/bulma.min.css">
-    
+
 </head>
 
 <body>
 
-<?php 
-    include '../parts/navbar.php'; 
-    include '../includes/database.php'; 
+    <?php
+    include '../parts/navbar.php';
+    include '../includes/database.php';
 
     $aktuellePflanze;
     $tempertureMin = 20;
@@ -23,95 +23,121 @@
     $moistureMin = 50;
     $moistureMax = 80;
 
-    $plant1 = getPlantData(1);
+    $plant = getPlantData(1);
     ?>
 
-    
-        <div class="field">
+
+    <div class="field columns is-centered">
+        <div class="column is-half">
             <label class="label">Wähle eine Pflanze</label>
             <div class="control">
                 <div class="select">
                     <select name="pflanze" id="pflanzeDropdown">
                         <option value="">Auswählen...</option>
-                        <option value=1>Pflanze 1</option>
-                        <option value=2>Pflanze 2</option>
+                        <?php
+                        for ($i = 1; $i <= getIDCount(); $i++) {
+                            echo '<option value="' . $i . '">Pflanze ' . $i . '</option>';
+                        }
+                        ?>
                     </select>
                 </div>
             </div>
         </div>
+    </div>
 
-        <form class="box">
-            <h5 class="title is-5">Zeitpunkt der Messung</h5>
+    <div class="block pl-6 pr-6">
+        <nav class="panel">
+            <p class="panel-heading">
+                Zeitpunkt der Messung
+            </p>
+            <p class="panel-tabs">
+                <?php
+                for ($i = 1; $i <= count($plant); $i++) {
+                    if ($i == 1) {
+                        echo '<a class="is-active" id="timestamp_' . $i . '"></a>';
+                        continue;
+                    }
+                    echo '<a id="timestamp_' . $i . '"></a>';
+                }
+                ?>
+            </p>
+        </nav>
+    </div>
+
+    <div class="block pl-6 pr-6">
+
+        <div class="block">
+            <h5 class="title is-5">Bodenleitfähigkeit in µS(Mikrosiemens) </h5>
             <div class="box" type="text">
-                <p id="datum"></p>
+                <p id="conduct_SOIL"></p>
             </div>
-            
-            
-                <h5 class="title is-5">Bodenleitfähigkeit in µS(Mikrosiemens) </h5>
-                <div class="box" type="text">
-                    <p id="conduct_SOIL"></p>
-                </div>
-            
-            
-                <h5 class="title is-5">Bodentemperatur in °C</h5>
-                <div class="box" type="text">
-                    <p id="temp_SOIL"></p>
-                </div>
+        </div>
 
-                <h5 class="title is-5">Bodenfeuchtigkeit in %</h5>
-                <div class="box" type="text">
-                    <p id="water_SOIL"></p>
-                </div>
-            
-                <h5 class="title is-5">Breitengrad</h5>
-                <div class="box" type="text">
-                    <p id="latitude"></p>
-                </div>
-            
-            
-                <h5 class="title is-5">Längengrad</h5>
-                <div class="box" type="text">
-                    <p id="longitude"></p>
-                </div>
-            
-        </form>
- 
+        <div class="block">
+            <h5 class="title is-5">Bodentemperatur in °C</h5>
+            <div class="box" type="text">
+                <p id="temp_SOIL"></p>
+            </div>
+        </div>
+
+        <div class="block">
+            <h5 class="title is-5">Bodenfeuchtigkeit in %</h5>
+            <div class="box" type="text">
+                <p id="water_SOIL"></p>
+            </div>
+        </div>
+
+        <div class="block">
+            <h5 class="title is-5">Breitengrad</h5>
+            <div class="box" type="text">
+                <p id="latitude"></p>
+            </div>
+        </div>
+
+        <div class="block">
+            <h5 class="title is-5">Längengrad</h5>
+            <div class="box" type="text">
+                <p id="longitude"></p>
+            </div>
+        </div>
+
+    </div>
+
 
     <script>
-         var plant1 = <?php echo json_encode($plant1); ?>;
+        var plant = <?php echo json_encode($plant); ?>;
 
-         function setFormData(plantData) {
+        function setFormData(plantData) {
 
             const sortedKeys = Object.keys(plantData).sort();
-
+            var i = 1;
             sortedKeys.forEach(key => {
                 const data = plantData[key];
-            console.log("Setting form data:", plantData); // Debugging-Informationen
-            document.getElementById('datum').innerHTML = data.date;
-            document.getElementById('conduct_SOIL').innerHTML = data.conduct_SOIL;
-            document.getElementById('temp_SOIL').innerHTML = data.temp_SOIL;
-            document.getElementById('latitude').innerHTML = data.latitude;
-            document.getElementById('longitude').innerHTML = data.longitude;
-            document.getElementById('water_SOIL').innerHTML = data.water_SOIL;
-        });
-    }
+                document.getElementById('timestamp_' + i).innerHTML = data.date;
+                i++;
+                document.getElementById('conduct_SOIL').innerHTML = data.conduct_SOIL;
+                document.getElementById('temp_SOIL').innerHTML = data.temp_SOIL;
+                document.getElementById('latitude').innerHTML = data.latitude;
+                document.getElementById('longitude').innerHTML = data.longitude;
+                document.getElementById('water_SOIL').innerHTML = data.water_SOIL;
+            });
+        }
 
-         // Select the dropdown element
+        // Select the dropdown element
         const dropdown = document.querySelector('#pflanzeDropdown');
 
-            // Add an event listener for the 'change' event
-        dropdown.addEventListener('change', function(event) {
-                // Get the selected value
+        // Add an event listener for the 'change' event
+        dropdown.addEventListener('change', function (event) {
+            // Get the selected value
             const selectedValue = event.target.value;
-                // Log the selected value to the console
-                if(selectedValue == 1){
-                    console.log('Selected value:', selectedValue);
-                    console.log(plant1);
-                    setFormData(plant1);
-                }
+            // Log the selected value to the console
+            if (selectedValue == 1) {
+                console.log('Selected value:', selectedValue);
+                setFormData(plant);
+            }
         });
 
-        
+
     </script>
 
     <style>
