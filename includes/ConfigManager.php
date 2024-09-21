c94bac50db0a768eeff8229688d07a4a9b92fdf8
xChezY <it@leonphan.dev>
2024-09-21 15:52:16 +0200

    cs fix

--- includes/ConfigManager.php
+++ includes/ConfigManager.php
@@ -4,48 +4,54 @@ namespace PlantMonitor;
 
 use Symfony\Component\Yaml\Yaml;
 
-class ConfigManager {
-
+class ConfigManager
+{
     private $plant_list;
-    
-    public function __construct() {
+
+    public function __construct()
+    {
 
         $this->plant_list = Yaml::parseFile(realpath(dirname(__DIR__, 1) . '/config.yaml'))['Plants'];
-        
-    }
-    
-    function getPlantsConfig() {
-        return $this->plant_list;    
+
     }
 
-    function getPlantConfig($plant_id) {
+    public function getPlantsConfig()
+    {
+        return $this->plant_list;
+    }
+
+    public function getPlantConfig($plant_id)
+    {
         return $this->plant_list[$plant_id];
     }
 
-    function getCount() {
+    public function getCount()
+    {
         return count($this->plant_list);
     }
 
-    function getMinConduct($plant_id) {
+    public function getMinConduct($plant_id)
+    {
         return $this->plant_list[$plant_id]['minConduct'];
     }
-    function getMaxConduct($plant_id) {
+    public function getMaxConduct($plant_id)
+    {
         return $this->plant_list[$plant_id]['maxConduct'];
     }
-    function getMinWater($plant_id) {
+    public function getMinWater($plant_id)
+    {
         return $this->plant_list[$plant_id]['minWater'];
     }
-    function getMaxWater($plant_id) {
+    public function getMaxWater($plant_id)
+    {
         return $this->plant_list[$plant_id]['maxWater'];
     }
-    function getMinTemp($plant_id) {
+    public function getMinTemp($plant_id)
+    {
         return $this->plant_list[$plant_id]['minTemp'];
     }
-    function getMaxTemp($plant_id) {
+    public function getMaxTemp($plant_id)
+    {
         return $this->plant_list[$plant_id]['maxTemp'];
     }
 }
-
-
-
-?>
