cd22d54e5d8573316a213d6e4bb8d6a09792283d
xChezY <it@leonphan.dev>
2024-10-19 11:40:47 +0200

    add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./parts/**/*.php",
+    "./features/**/*.php",
+    "./contact/**/*.php",
+    "./index.php",
+    ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
