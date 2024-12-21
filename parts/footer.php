66c6f25dc921fd4090c1349e5ed4b1e1c00e949e
Merlin Hofmann | Merona <merlin@merona.de>
2024-12-21 11:59:15 +0100

    clean up for presentation add data privacy policy

--- parts/footer.php
+++ parts/footer.php
@@ -1,40 +1,17 @@
+
+
 <footer class="footer">
     <section class="section">
-        <div class="fixed-grid has-9-cols">
-            <div class="grid">
-                <div class="cell is-col-start-3 is-col-span-2">
-                    <div class="card">
-                        <div class="card-image">
-                            <figure class="image is-5by3">
-                                <img src="https://bulma.io/assets/images/placeholders/1280x960.png"
-                                    alt="Placeholder image" />
-                            </figure>
+                <div class="fixed-grid has-1-cols">
+                    <div class="grid">
+                        <div class="cell">
+                            <a class="subtitle has-text-black" href="<?php echo $_ENV['IMPRESSUM_LINK'] ?? "" ?>">Impressum </a>
                         </div>
-                    </div>
-                    <div class="content">
-                        <ul class="mt-2">
-                            <li>E-Mail Adresse</li>
-                            <li class="mt-2">Telefonnummer</li>
-                        </ul>
+                        <div class="cell">
+                            <a class="subtitle has-text-black" href"<?php echo $_ENV['DATENSCHUTZ_LINK'] ?? "" ?>">Datenschutz</a>
+                        </div>
                     </div>
 
                 </div>
-                <div class="cell is-col-start-6 is-col-span-2">
-                    <p class="subtitle has-text-white is-size-4">Sitemap</p>
-                    <div class="content">
-                        <ul>
-                            <li><a class="has-text-grey-light" href="/">Home</a></li>
-                            <li class="mt-2"><a class="has-text-grey-light" href="/features">Features</a></li>
-                            <li class="mt-2"><a class="has-text-grey-light" href="/#about">About us</a></li>
-                            <li class="mt-2"><a class="has-text-grey-light" href="/contact">Kontakt</a></li>
-                        </ul>
-                        <p class="subtitle has-text-white">Impressum</p>
-                        <p class="subtitle has-text-white">Datenschutz</p>
-                    </div>
-                </div>
-            </div>
     </section>
-    <div class="container">
-        <p>@Copyright blabla</p>
-    </div>
 </footer>
